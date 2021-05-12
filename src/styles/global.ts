239b09c8efe4e4484193956679b793048cc9b2aa
Antonio Raposo <antoniocarlosraposo93@gmail.com>
2021-05-12 16:41:34 -0300

    adding header component

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -36,8 +36,20 @@ export const GlobalStyle = createGlobalStyle`
     }
 
 
+
     button {
         cursor: pointer;
+    }
+
+
+    body,input,textarea, button {
+        font-family: 'Poppins', sans-serif;
+        font-weight: 400;
+
+
+        h1,h2,h3,h4,h5,h6,strong {
+            font-weight:600;
+        }
     }
 
     [disabled]{
